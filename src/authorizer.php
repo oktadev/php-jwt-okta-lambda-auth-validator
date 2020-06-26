f785e0e05587584196471f6df5fb2e1adeb06581
David Marín <david.marin@toptal.com>
2020-06-26 18:22:17 +0200

    Removing 'Bearer ' prefix from received Authorization header

--- src/authorizer.php
+++ src/authorizer.php
@@ -5,13 +5,16 @@ use Firebase\JWT\ExpiredException;
 use Firebase\JWT\JWT;
 use Firebase\JWT\SignatureInvalidException;
 
-static $signingKey = '';
-
 function authorizer($data) {
 
 	$type = $data['type'];
 	$jwt = $data['authorizationToken'];
 	$method = $data['methodArn'];
+
+	// Remove the "Bearer " prefix from $jwt, if it exists
+	if ( strpos( $jwt,'Bearer ' ) === 0 ) {
+		$jwt = str_replace( 'Bearer ', '', $jwt );
+	}
 
 	$key_manager = OktaLambdaAuth\KeyManager::instance();
 	$keys = $key_manager->getKeys();
@@ -24,11 +27,14 @@ function authorizer($data) {
 			$decoded_token = JWT::decode( $jwt, $key, array( 'RS256' ) );
 			unset( $error );
 			$jwt_valid = true;
-		} catch ( ExpiredException | BeforeValidException $e) {
+		} catch ( ExpiredException | BeforeValidException $e ) {
 			$error = 'Token expired, or used before its validity';
 			break;
-		} catch ( SignatureInvalidException $e) {
+		} catch ( SignatureInvalidException $e ) {
 			$error = 'Token not valid';
+			continue;
+		} catch ( Exception $e ) {
+			$error = 'Token problem: ' . $e->getMessage();
 			continue;
 		}
 	}
