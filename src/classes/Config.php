d07bf1d010de2af0976cde5b967cb257602551c3
David Marín <david.marin@toptal.com>
2020-06-26 10:27:07 +0200

    Uploading possible first full working version, before tests

--- src/classes/Config.php
+++ src/classes/Config.php
@@ -7,9 +7,9 @@ namespace OktaLambdaAuth;
 class Config {
 
 	// Define here your Okta server hostname, like 'dev-XXXXX.okta.com' or 'xxxxx.okta-emea.com'
-	const OKTA_SERVER_HOSTNAME = 'dev-154263.okta.com';
+	const OKTA_SERVER_HOSTNAME = 'dev-XXXXX.okta.com';
 
 	// Define here your Okta app's client ID
-	const OKTA_APP_CLIENT_ID = '0oah6nzjiX7TZj6KC4x6';
+	const OKTA_APP_CLIENT_ID = 'XXXXXXXXXXXXXXXXXXXX';
 
 }
